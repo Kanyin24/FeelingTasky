e25665ce321e04d966fbaae05573d722448f823e
Aiden Fox-Ivey <71291879+Foxnaut@users.noreply.github.com>
2021-07-03 13:49:09 -0400

    Moved to one js file.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,67 @@
+function add_x () {
+  // creates the close button
+  var myNodelist = document.getElementsByTagName("LI");
+  var i;
+  for (i = 0; i < myNodelist.length; i+=1) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "closetag";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+  }
+
+  // makes the close button cause the div to display when clicked
+  var close = document.getElementsByClassName("closetag");
+  var i;
+  for (i = 0; i < close.length; i+=1) {
+    // function prototype
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+function addTaskToList () {
+  let task = document.getElementById("zero").value.trim();
+  
+  if (task === ' ' || task === '') {
+    alert('Please add a task! Blank tasks aren\'t accepted.');
+  } else {
+    console.log(task);
+  }
+
+
+}
+
+function submitTasks () {
+  // gather unordered list into array
+  let array = [];
+  let elements = document.getElementsByTagName("LI");
+  let j = 0;
+
+  for (j=0; j<elements.length; j += 1) {
+    array.push(elements[j].innerHTML);
+  }
+
+  return array  
+  // redirect to the next page
+  window.location.href="../views/dragBar.html";
+ }
+
+
+function add_listeners () {
+  // listen for click on add button and call addTaskToList
+  document.getElementById('add').addEventListener('click', addTaskToList);
+
+  // listen for click on next button and call submitTasks
+  document.getElementById('submit').addEventListener('click', submitTasks);
+}
+
+add_listeners()
+add_x()
+
 const listA = [], listB = [], listC = [];
-// SECTION 1
-
-// let module = require("./stageone.js");
-// let array = module.array;
-
-// console.log(array);
-
-
-
-// SECTION 2
-
 const listOftasks = ["one", "two", "three", "four", "five", "six", "seven", "eight"];
 
   // when the page loads print all the tasks
--- render.js
+++ render.js
@@ -1,30 +1,17 @@
-const { app, BrowserWindow, Notification } = require('electron');
-// CommonJS modules import here
-// app => controls event lifecycle
-// BrowserWindow => manages the application window
-
-// const NOTIFICATION_TITLE = 'Crappy notification';
-// const NOTIFICATION_BODY = 'Notification from the rendered process. click to log to console.';
-// const CLICK_MESSAGE = 'Notification clicked';
+const { app, BrowserWindow } = require('electron');
+const fs = require('fs');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 850,
-    height: 650,
+    width: 1920,
+    height: 1080,
 
   });
 
   win.loadFile('./views/index.html');
 }
 
-// function showNotification() {
-//   new Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY })
-//     .show()
-//   // this should do something maybe
-//     .onclick = () => console.log(CLICK_MESSAGE).catch(console.error('error'));
-// }
 
-app.whenReady().then(createWindow); // .then(showNotification)
+app.whenReady().then(createWindow); 
 
-// need logic for quiting the program
-// kinda shitty at the moment
+
